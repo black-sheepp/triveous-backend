0bdec7764204520e89f43baa6b6b62288fd64c4b
Shivam <shivamguptanitw@gmail.com>
2024-02-24 13:31:30 +0545

    Project's MVC structures and setup is ready

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const db = require("./config/mongoose");
+const bodyParser = require("body-parser");
 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", require("./routes/index"));
 
