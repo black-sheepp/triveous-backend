7ca82d5bc9b75c847ce2144dfba61816b38ffd70
Shivam <shivamguptanitw@gmail.com>
2024-02-24 13:29:08 +0545

    Routes and Mongoose setup

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
+const db = require("./config/mongoose");
 
+
+app.use("/", require("./routes/index"));
 
 app.listen(PORT, () => {
 	console.log("Server listening on port " + PORT);
