26f8f6eac7cb0aea73ae19511de13dde5bb21381
Shivam <shivamguptanitw@gmail.com>
2024-02-24 13:23:13 +0545

    Express Server is setup and running successfully on local machine

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+
+app.listen(PORT, () => {
+	console.log("Server listening on port " + PORT);
+});
